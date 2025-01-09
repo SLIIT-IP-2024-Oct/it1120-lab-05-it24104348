6ce6a14d6eac9fd66aec43bf4907c3b3725505ba
it24104348 <it24104348@my.sliit.lk>
2025-01-09 10:44:09 +0530

    Add files via upload

---
+++ Q2/IT24104348Lab5Q2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class IT24104348Lab5Q2 {
+    public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of new members introduced: ");
+          int newMembers = scanner.nextInt();
+
+       if (newMembers < 0) {
+            System.out.println("Invalid");
+           } else {
+            String prize;
+            switch (newMembers) {
+                case 0: prize = "No Prize"; break;
+                case 1: prize = "Pen"; break;
+                case 2: prize = "Umbrella"; break;
+                case 3: prize = "Bag"; break;
+                case 4: prize = "Travelling Chair"; break;
+                default: prize = "Headphone"; break;
+            }
+            System.out.println("Prize: " + prize);
+        }
+    }
+}
